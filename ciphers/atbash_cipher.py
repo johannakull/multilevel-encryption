ae6c094779236d3228a985398e100dafedb63ab5
johanna <johanna.kull@live.de>
2025-03-22 18:23:05 +0000

    add type hints and rename function and variables

--- ciphers/atbash_cipher.py
+++ ciphers/atbash_cipher.py
@@ -7,19 +7,19 @@ class AtbashCipher(ChangeBase):
     def __init__(self, base: str):
         super().__init__(base)
 
-    def generate_new_text(self):
-        new_text = ""
+    def _generate_atbash_text(self) -> str:
+        atbash_text = ""
         for char in self.base:
             if char in ALPHABET_SET:
                 i = ALPHABET.index(char)
                 new_i = 25 - i
-                new_text += ALPHABET[new_i]
+                atbash_text += ALPHABET[new_i]
             else:
-                new_text += char
-        return new_text
+                atbash_text += char
+        return atbash_text
 
-    def encrypt(self):
-        return self.generate_new_text()
+    def encrypt(self) -> str:
+        return self._generate_atbash_text()
 
-    def decrypt(self):
-        return self.generate_new_text()
+    def decrypt(self) -> str:
+        return self._generate_atbash_text()
