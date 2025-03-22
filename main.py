6d0bc7086dc3ca06fca783df1bdc0af79d1cb79b
johanna <johanna.kull@live.de>
2025-03-22 10:57:11 +0000

    add caesar cipher class structure

--- main.py
+++ main.py
@@ -13,3 +13,22 @@ class ChangeBase(ABC):
     @abstractmethod
     def decrypt(self):
         return
+
+
+class CaesarCipher(ChangeBase):
+
+    def __init__(self, base):
+        super().__init__(base)
+
+    def encrypt(self) -> str:
+        return "encrypted string"
+
+    def decrypt(self) -> str:
+        return "decrypted string"
+
+
+# base_string = "hello world"
+# caesar = CaesarCipher(base_string)
+# encrypted_base_string = caesar.encrypt()
+#
+# print(encrypted_base_string)
