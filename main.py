2be416c20e31cdb33202d9545d89d21ac4688996
johanna <johanna.kull@live.de>
2025-03-22 18:44:45 +0000

    add some basic control flow

--- main.py
+++ main.py
@@ -1,68 +1,24 @@
-from abc import ABC, abstractmethod
-
-ALPHABET = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
-ALPHABET_SET = set(ALPHABET)
-
-class ChangeBase(ABC):
-
-    def __init__(self, base):
-        self.base = base
-        self.cipher = None
-
-    @abstractmethod
-    def encrypt(self):
-        return
-
-    @abstractmethod
-    def decrypt(self):
-        return
+from ciphers.caesar_cipher import CaesarCipher
+from ciphers.atbash_cipher import AtbashCipher
 
 
-class CaesarCipher(ChangeBase):
+def main():
+    original_text = input("Enter the text to be encoded/decoded: ")
 
-    def __init__(self, base: str, shift_number: int):
-        super().__init__(base)
-        self.shift_number = shift_number
+    print("\nWhich of the following ciphers would you like to use?\n")
 
-    def _shift_forward(self, index):
-        new_index = index + self.shift_number
-        if new_index > len(ALPHABET) - 1:
-            new_index -= len(ALPHABET)
-        return new_index
+    cipher_options = {
+        "1": "Caesar Cipher",
+        "2": "Atbash Cipher",
+    }
 
-    def _shift_backward(self, index):
-        new_index = index - self.shift_number
-        if new_index < 0:
-            new_index += len(ALPHABET)
-        return new_index
+    for num, cipher in cipher_options.items():
+        print(f"{num}: {cipher}")
 
-    def encrypt(self) -> str:
-        encrypted_text = ""
-        for char in self.base:
-            if char in ALPHABET_SET:
-                i = ALPHABET.index(char)
-                new_i = self._shift_forward(i)
-                encrypted_text += ALPHABET[new_i]
-            else:
-                encrypted_text += char
-        return encrypted_text
+    cipher_choice = input("\nEnter the number of the cipher: ")
 
-    def decrypt(self) -> str:
-        decrypted_text = ""
-        for char in self.base:
-            if char in ALPHABET_SET:
-                i = ALPHABET.index(char)
-                new_i = self._shift_backward(i)
-                decrypted_text += ALPHABET[new_i]
-            else:
-                decrypted_text += char
-        return decrypted_text
+    caesar = CaesarCipher(original_text)
 
 
-original = "HELLO WORLD"
-caesar = CaesarCipher(original, 3)
-print(caesar.encrypt())
-
-encrypted = "KHOOR ZRUOG"
-caesar = CaesarCipher(encrypted, 3)
-print(caesar.decrypt())
+if __name__ == "__main__":
+    main()
