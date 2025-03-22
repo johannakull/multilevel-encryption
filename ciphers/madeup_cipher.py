d0649025abecdd4e6c5c82f7362b4a9332b7a5de
johanna <johanna.kull@live.de>
2025-03-22 19:27:44 +0000

    add made up cipher

---
+++ ciphers/madeup_cipher.py
@@ -0,0 +1,34 @@
+from ciphers.change_base import ChangeBase
+from constants import ALPHABET, ALPHABET_SET
+
+
+class MadeUpCipher(ChangeBase):
+
+    def __init__(self, base):
+        super().__init__(base)
+
+    def encrypt(self):
+        encrypted_text = ""
+        for char in self.base:
+            if char in ALPHABET_SET:
+                new_char = str(ALPHABET.index(char) + 1)
+                encrypted_text += f"{new_char} "
+            elif char == " ":
+                encrypted_text += "0 "
+            else:
+                encrypted_text += f"{char} "
+        return encrypted_text
+
+    def decrypt(self):
+        decrypted_text = ""
+        for char in self.base.split(" "):
+            try:
+                index = int(char) - 1
+            except ValueError:
+                decrypted_text += char
+            else:
+                if char == "0":
+                    decrypted_text += " "
+                else:
+                    decrypted_text += ALPHABET[index]
+        return decrypted_text
