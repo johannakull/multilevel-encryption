a6ffc10b5bd9668b0c5bef6596ae26d7aa5eaa0e
johanna <johanna.kull@live.de>
2025-03-28 20:16:05 +0000

    validate shift number input in caesar cipher

--- ciphers/caesar_cipher.py
+++ ciphers/caesar_cipher.py
@@ -6,17 +6,22 @@ class CaesarCipher(Cipher):
 
     def __init__(self, original_text: str):
         super().__init__(original_text)
-        self.shift_number = None
 
     name = "Caesar Cipher"
 
     def _shift_chars(self, multiplier: int):
-        self.shift_number = int(input("Enter a shift number: ")) * multiplier
+        while True:
+            try:
+                shift_number = int(input("Enter a shift number: ")) * multiplier
+            except ValueError:
+                print("Please enter an integer.")
+            else:
+                break
         shifted_text = ""  # do this as a list instead
         for char in self.original_text:
             if char in LETTER_TO_INDEX_MAP:
                 i = LETTER_TO_INDEX_MAP[char]
-                new_i = (i + self.shift_number) % len(LETTER_TO_INDEX_MAP)
+                new_i = (i + shift_number) % len(LETTER_TO_INDEX_MAP)
                 shifted_text += INDEX_TO_LETTER_MAP[new_i]
             else:
                 shifted_text += char
