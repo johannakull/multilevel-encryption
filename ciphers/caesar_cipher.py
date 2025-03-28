1cab5ef40f9d7a649371b386a55fb455deea75f2
johanna <johanna.kull@live.de>
2025-03-28 20:56:29 +0000

    use list instead of string for performance

--- ciphers/caesar_cipher.py
+++ ciphers/caesar_cipher.py
@@ -9,7 +9,7 @@ class CaesarCipher(Cipher):
 
     name = "Caesar Cipher"
 
-    def _shift_chars(self, multiplier: int):
+    def _shift_chars(self, multiplier: int) -> str:
         while True:
             try:
                 shift_number = int(input("Enter a shift number: ")) * multiplier
@@ -17,15 +17,15 @@ class CaesarCipher(Cipher):
                 print("Please enter an integer.")
             else:
                 break
-        shifted_text = ""  # do this as a list instead
+        shifted_text = []
         for char in self.original_text:
             if char in LETTER_TO_INDEX_MAP:
                 i = LETTER_TO_INDEX_MAP[char]
                 new_i = (i + shift_number) % len(LETTER_TO_INDEX_MAP)
-                shifted_text += INDEX_TO_LETTER_MAP[new_i]
+                shifted_text.append(INDEX_TO_LETTER_MAP[new_i])
             else:
-                shifted_text += char
-        return shifted_text
+                shifted_text.append(char)
+        return "".join(shifted_text)
 
     def encrypt(self) -> str:
         return self._shift_chars(1)
